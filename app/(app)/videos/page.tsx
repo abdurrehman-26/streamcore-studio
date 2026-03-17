032f16f5c11e03cff87814a78d0716255d700b2a
abdurehman-26 <ar2522006@gmail.com>
2026-03-17 16:22:36 +0500

    add videos page

---
+++ app/(app)/videos/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { useQuery } from "@tanstack/react-query"
+import { api } from "@/streamcore-api"
+import { Skeleton } from "@/components/ui/skeleton"
+import { VideoItem } from "@/components/video-item"
+
+export default function VideosPage() {
+  const query = useQuery({
+    queryKey: ['videos'],
+    queryFn: () => api.videos.getVideos(),
+  })
+  return (
+    <div className="ml-64 p-4">
+      <div className="mb-4 bg-accent p-2 rounded">
+        <h1 className="text-2xl font-bold">Videos</h1>
+        <p className="text-sm text-muted-foreground">
+          Manage your videos here. You can view, edit, or delete your videos.
+        </p>
+      </div>
+      {query.isLoading && (
+        Array.from({ length: 5 }).map((_, index) => (
+          <Skeleton key={index} className="h-65 w-60 mb-2" />
+        ))
+      )}
+      {query.isError && <p>Error occurred while fetching videos.</p>}
+      {query.data && (
+        <ul className="grid grid-cols-4 gap-4">
+          {query.data.map((video) => (
+            <VideoItem key={video.videoId} video={video} />
+          ))}
+        </ul>
+      )}
+    </div>
+  )
+}
