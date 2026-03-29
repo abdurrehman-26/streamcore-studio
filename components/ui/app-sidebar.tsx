cfa7e735327b2c17fa7ae8b9f992cdc20c3c1de9
abdurehman-26 <ar2522006@gmail.com>
2026-03-29 15:47:49 +0500

    refactor the repeating buttom components to map over predefined array to render navItems

--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -1,17 +1,20 @@
-import { Settings2, User, Video } from "lucide-react";
+import { Video } from "lucide-react";
 import { AppNavButton } from "../app-nav-button";
 
 export default function AppSidebar() {
+  const navItems = [
+    { href: "/videos", icon: <Video />, label: "Videos" },
+  ]
   return (
     <div className="w-64 bg-gray-800 text-white h-screen fixed p-4">
-      <div className="mb-4 p-4">
+      <div className="mb-4 p-4 select-none">
         <h2 className="text-xl font-bold mb-4">StreamCore Studio</h2>
       </div>
       <nav>
         <ul>
-          <AppNavButton href="/videos" icon={<Video />} label="Videos" />
-          <AppNavButton href="/profile" icon={<User />} label="Profile" />
-          <AppNavButton href="/settings" icon={<Settings2 />} label="Settings" />
+          {navItems.map((item) => (
+            <AppNavButton key={item.href} href={item.href} icon={item.icon} label={item.label} />
+          ))}
         </ul>
       </nav>
     </div>
