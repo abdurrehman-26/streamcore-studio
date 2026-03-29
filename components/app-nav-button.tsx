ef9bc5e1cfaa0874bd47defa5dd81f864c47dcc2
abdurehman-26 <ar2522006@gmail.com>
2026-03-29 15:45:15 +0500

    make the nav link highlighted when active and fix the link span

--- components/app-nav-button.tsx
+++ components/app-nav-button.tsx
@@ -1,15 +1,21 @@
+"use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 export function AppNavButton({ href, icon, label }: { href: string; icon: React.ReactNode; label: string }) {
+  const path = usePathname()
+  const isActive = path.includes(href)
   return (
-    <Button asChild variant="ghost" className="w-full mb-2 px-4 py-6">
-      <li>
-        <Link href={href} className="flex gap-1 rounded text-left w-full">
-          {icon}
-          <span className="ml-2">{label}</span>
-        </Link>
-      </li> 
-    </Button>
+    <li className="flex">
+      <Button asChild variant="ghost" className={`w-full mb-2 px-4 py-6 hover:bg-gray-700 hover:text-white ${isActive ? "bg-gray-700" : ""}`}>
+          <Link href={href} className="flex gap-1 rounded text-left w-full">
+            <span className="flex items-center mr-auto">
+              {icon}
+              <span className="ml-2">{label}</span>
+            </span>
+          </Link>
+      </Button>
+    </li> 
   )
 }
