f000e281dd906910b58202b8eeb8d65c95f0f6b4
abdurehman-26 <ar2522006@gmail.com>
2026-04-21 19:25:43 +0500

    remove search input from app header

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -1,18 +1,12 @@
-import { Bell, Search, Settings } from "lucide-react";
-import { Input } from "./ui/input";
+"use client";
+import { Bell, Settings } from "lucide-react";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function AppHeader() {
   return (
     <div className="flex items-center justify-between shadow-sm p-4 sticky top-0 bg-background z-10">
-      <div className="relative w-full max-w-md">
-        <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-        <Input
-          className="pl-10"
-          placeholder="Search videos..."
-        />
-      </div>
+      <p className="text-lg font-semibold text-gray-500">Workspace 1</p>
       <div className="flex items-center gap-4">
         <Button variant="outline" size="icon">
           <Bell className="h-5 w-5" />
